a045b9dd44bebc7fb2d1592771b9f9c61b13c9c8
shmuly11 <shmuly11@gmail.com>
2020-12-11 12:08:15 -0500

    manager class started

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,19 @@
 class Employee
+    @@all = []
+
+
+    attr_reader :name, :manager
+    attr_accessor :salary
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,22 @@
 class Manager
+    @@all = []
+    attr_reader :name, age, department
+
+    def initialize (name, age, department)
+        @name = name
+        @age = age
+        @department = department
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select
+        
+    end
 
 end
